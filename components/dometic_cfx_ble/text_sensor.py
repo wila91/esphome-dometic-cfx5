ebd4bad53e3839fa0ad7d3d85fada846800453e2
wila91 <102232162+wila91@users.noreply.github.com>
2026-08-10 21:49:09 +0300

    Update text_sensor.py

--- components/dometic_cfx_ble/text_sensor.py
+++ components/dometic_cfx_ble/text_sensor.py
@@ -19,4 +19,5 @@ async def to_code(config):
 
     parent = await cg.get_variable(config[CONF_DOMETIC_CFX_BLE_ID])
     cg.add(var.set_parent(parent))
-    cg.add(var.set_topic(config[CONF_TYPE])) 
+    cg.add(var.set_topic(config[CONF_TYPE]))
+    cg.add(parent.add_entity(config[CONF_TYPE], var))
