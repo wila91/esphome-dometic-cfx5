2808b7a9d0f89df75808dea10c9f5f907f9fccc8
wila91 <102232162+wila91@users.noreply.github.com>
2026-08-10 10:59:56 +0300

    Update binary_sensor.py

--- components/dometic_cfx_ble/binary_sensor.py
+++ components/dometic_cfx_ble/binary_sensor.py
@@ -1,17 +1,22 @@
 import esphome.codegen as cg
+import esphome.config_validation as cv
 from esphome.components import binary_sensor
 from esphome.const import CONF_ID, CONF_TYPE
-from . import CONF_DOMETIC_CFX_BLE_ID, entity_schema
+from . import dometic_cfx_ble_ns, CONF_DOMETIC_CFX_BLE_ID, entity_schema
 
 DEPENDENCIES = ["dometic_cfx_ble"]
 
-CONFIG_SCHEMA = entity_schema("binary_sensor").extend(binary_sensor.binary_sensor_schema())
+DometicCfxBleBinarySensor = dometic_cfx_ble_ns.class_("DometicCfxBleBinarySensor", binary_sensor.BinarySensor, cg.PollingComponent)
+
+CONFIG_SCHEMA = entity_schema("binary_sensor").extend(binary_sensor.binary_sensor_schema(DometicCfxBleBinarySensor)).extend({
+    cv.GenerateID(): cv.declare_id(DometicCfxBleBinarySensor)
+})
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
     await binary_sensor.register_binary_sensor(var, config)
 
-    # Link the binary sensor directly to the main Dometic Bluetooth Hub
     parent = await cg.get_variable(config[CONF_DOMETIC_CFX_BLE_ID])
     cg.add(var.set_parent(parent))
     cg.add(var.set_type(config[CONF_TYPE]))
