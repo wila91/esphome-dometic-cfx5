991bb3e3b2a8185d58303d25f15212feed640309
wila91 <102232162+wila91@users.noreply.github.com>
2026-08-10 10:52:22 +0300

    Update text_sensor.py

--- components/dometic_cfx_ble/text_sensor.py
+++ components/dometic_cfx_ble/text_sensor.py
@@ -5,12 +5,10 @@ from . import CONF_DOMETIC_CFX_BLE_ID, entity_schema
 
 DEPENDENCIES = ["dometic_cfx_ble"]
 
-# We use the entity_schema from __init__.py and extend it with ESPHome's text sensor schema
 CONFIG_SCHEMA = entity_schema("text_sensor").extend(text_sensor.text_sensor_schema())
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
     await text_sensor.register_text_sensor(var, config)
 
     # Link the text sensor directly to the main Dometic Bluetooth Hub
